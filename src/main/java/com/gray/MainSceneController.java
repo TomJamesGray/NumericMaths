88485f59e33abf58a2042f98c27bae711d8e9c41
Tom Gray <tomg77@yahoo.co.uk>
2021-02-05 18:24:18 +0000

    Showing/Hiding appropriate screen based on menu selection

--- src/main/java/com/gray/MainSceneController.java
+++ src/main/java/com/gray/MainSceneController.java
@@ -1,4 +1,41 @@
 package com.gray;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class MainSceneController {
+    @FXML
+    private VBox eqSolvingWindow;
+    @FXML
+    private VBox welcomeWindow;
+    @FXML
+    private VBox calculationsWindow;
+
+    private VBox[] windows;
+
+    @FXML private void initialize(){
+        this.windows = new VBox[]{eqSolvingWindow, welcomeWindow, calculationsWindow};
+        for (VBox window:windows){
+            window.managedProperty().bind(window.visibleProperty());
+        }
+    }
+
+    private void hideAllWindows(){
+        for (VBox window : windows){
+            window.setVisible(false);
+        }
+    }
+
+    @FXML
+    private void loadEquationSolving(){
+        hideAllWindows();
+        eqSolvingWindow.setVisible(true);
+    }
+
+    @FXML
+    private void loadCalculations(){
+        hideAllWindows();
+        calculationsWindow.setVisible(true);
+    }
+
 }
